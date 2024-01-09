da24b289c5495b170f20bda82bfa02a4dd8f12c6
vanessa <NovaDReaper@yahoo.com>
2024-01-09 15:59:22 -0500

    upload

--- word_frequency.js
+++ word_frequency.js
@@ -1,48 +1,38 @@
-const fs = require('fs')
-const path = require('path')
+const { count } = require("console")
+const fs = require("fs")
+const path = require("path")
 const filePath = process.argv[2]
 
-const STOP_WORDS = [
-  'a',
-  'an',
-  'and',
-  'are',
-  'as',
-  'at',
-  'be',
-  'by',
-  'for',
-  'from',
-  'has',
-  'he',
-  'i',
-  'in',
-  'is',
-  'it',
-  'its',
-  'of',
-  'on',
-  'that',
-  'the',
-  'to',
-  'were',
-  'will',
-  'with',
-]
+const STOP_WORDS = ["a", "an", "and", "are", "as", "at", "be", "by", "for", "from", "has", "he", "i", "in", "is", "it", "its", "of", "on", "that", "the", "to", "were", "will", "with"]
 
 function printWordFreq(file, callback) {
   // Read in `file` and print out the frequency of words in that file.
-  fs.readFile(file, 'utf8', (err, data) => {
+  fs.readFile(file, "utf8", (err, data) => {
     if (err) {
-      console.error('Error reading the file:', err)
+      console.error("Error reading the file:", err)
       process.exit(1)
     }
     // TODO: write code to count the words in the file
-    console.log('Initial data read from file: ', data)
-    callback('Your word count results should be passed in to this callback')
+
+    const textToCount = data.toLowerCase().split(/\W+/).sort()
+    const countObject = {}
+
+    for (word of textToCount) {
+      if (STOP_WORDS.includes(word)) {
+        continue
+      }
+      if (word in countObject) {
+        countObject[word]++
+      } else {
+        countObject[word] = 1
+      }
+    }
+
+    console.log("Initial data read from file: ", data)
+    callback(countObject)
   })
 }
 
-printWordFreq(filePath, (wordCount) => {
-  console.log('The results from your word counts:', wordCount)
+printWordFreq(filePath, wordCount => {
+  console.log("The results from your word counts:", wordCount)
 })
